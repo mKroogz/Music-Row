feadae193d5f6194b76fcd3fe408dbc97f93c8b0
Matthew Kroeger <mattk932009@gmail.com>
2020-01-31 11:26:56 -0600

    initial commit

---
+++ music.js
@@ -0,0 +1,38 @@
+const createFunkArtist = (name, age) => {
+    return {
+        "name": name,
+        "age" : age,
+    }
+}
+
+const createRapArtist = (name, age) => {
+    return {
+        "name": name,
+        "age" : age,
+    }
+}
+
+const createCountryArtist = (name, age) => {
+    return {
+        "name": name,
+        "age" : age,
+    }
+}
+
+const createBluegrassArtist = (name, age) => {
+    return {
+        "name": name,
+        "age" : age,
+    }
+}
+
+const createPopArtist = (name, age) => {
+    return {
+        "name": name,
+        "age" : age,
+    }
+}
+
+const jumpStopRecords = [createFunkArtist("Dre Funkz", 25), createRapArtist("Dusta Grimes", 21), createRapArtist("Loyonce Branis", 27)]
+const chattenRecords = [createCountryArtist("Bruce Atkins", 23), createCountryArtist("Avilee Dallas", 19), createBluegrassArtist("Bartholomew Danielson", 23)]
+const polarRecords = [createPopArtist("Jensen Brown", 20), createPopArtist("Austin Kinkaid", 22)]
